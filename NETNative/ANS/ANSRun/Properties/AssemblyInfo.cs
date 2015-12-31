5556e9f2e73ff9f0b80e209399a10ff0bcd08976
sldeskins <sldeskins@ymail.com>
2015-12-31 11:01:10 -0500

    Change build configuration

--- NETNative/ANS/ANSRun/Properties/AssemblyInfo.cs
+++ NETNative/ANS/ANSRun/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ANSRun")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("WIB @sldeskins")]
 [assembly: AssemblyProduct("ANSRun")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
